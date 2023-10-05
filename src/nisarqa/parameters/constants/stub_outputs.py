b842915c2c22dff1e9919661572cbb98668c052f
Samantha C. Niemoeller <samantha.c.niemoeller@jpl.nasa.gov>
2023-10-05 09:33:44 -0700

    reduce stub file placeholder text.

--- src/nisarqa/parameters/constants/stub_outputs.py
+++ src/nisarqa/parameters/constants/stub_outputs.py
@@ -10,95 +10,13 @@ import nisarqa
 
 objects_to_skip = nisarqa.get_all(name=__name__)
 
-LOG_TXT = """PLACEHOLDER FILE ONLY -- NOT ACTUAL LOG OUTPUTS
-2022-05-02 18:51:40,466, INFO, QA, misc, 100000 '/projects/QualityAssurance/verify_rslc.py':73, "N/A: Successfully parsed XML file /projects/QualityAssurance/xml/nisar_L1_RSLC.xml"
-2022-05-02 18:51:40,469, INFO, QA, misc, 100000 '/projects/QualityAssurance/verify_rslc.py':79, "N/A: Opening file /home/niemoell/dat/fromJoanne_05022022/rslc_REE_testarea134/output_rslc/rslc.h5 with xml spec /projects/QualityAssurance/xml/nisar_L1_RSLC.xml"
-2022-05-02 18:51:40,472, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':37, "N/A: Opening file /home/niemoell/dat/fromJoanne_05022022/rslc_REE_testarea134/output_rslc/rslc.h5"
-2022-05-02 18:51:40,479, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':65, "N/A: LSAR Start time 2023-07-01T00:08:13.501873000"
-2022-05-02 18:51:40,482, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':65, "N/A: SSAR Start time 9999-99-99T99:99:99"
-2022-05-02 18:51:40,485, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':67, "N/A: Found band LSAR"
-2022-05-02 18:51:40,488, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/HdfGroup.py':23, "N/A: Initializing HDF Group LSAR Swath with /science/LSAR/RSLC/swaths"
-2022-05-02 18:51:40,491, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/HdfGroup.py':23, "N/A: Initializing HDF Group LSAR Metadata with /science/LSAR/RSLC/metadata"
-2022-05-02 18:51:40,494, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/HdfGroup.py':23, "N/A: Initializing HDF Group LSAR Identification with /science/LSAR/identification/"
-2022-05-02 18:51:40,497, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':64, "N/A: SSAR not present"
-2022-05-02 18:51:40,500, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':84, "N/A: Found LSAR FrequencyA"
-2022-05-02 18:51:40,504, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':84, "N/A: Found LSAR FrequencyB"
-2022-05-02 18:51:40,509, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':178, "N/A: Checking polarization  in <class 'h5py._hl.group.Group'>"
-2022-05-02 18:51:40,513, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':178, "N/A: Checking polarization  in <class 'h5py._hl.group.Group'>"
-2022-05-02 18:51:40,518, DEBUG, QA, misc, 101000 '/projects/QualityAssurance/quality/NISARFile.py':298, "N/A: nsubswaths <class 'int'> = 1"
-2022-05-02 18:51:40,522, DEBUG, QA, misc, 101000 '/projects/QualityAssurance/quality/NISARFile.py':298, "N/A: nsubswaths <class 'int'> = 1"
-2022-05-02 18:51:40,525, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':311, "N/A: LSAR: no_look=['frequencyA/validSamplesSubSwath2', 'frequencyA/validSamplesSubSwath3', 'frequencyA/validSamplesSubSwath4', 'frequencyA/HHHH', 'frequencyA/HHHV', 'frequencyA/HHVH', 'frequencyA/HHVV', 'frequencyA/HVHH', 'frequencyA/HVHV', 'frequencyA/HVVH', 'frequencyA/HVVV', 'frequencyA/VHHH', 'frequencyA/VHHV', 'frequencyA/VHVH', 'frequencyA/VHVV', 'frequencyA/VVHH', 'frequencyA/VVHV', 'frequencyA/VVVH', 'frequencyA/VVVV', 'frequencyA/RHRH', 'frequencyA/RHRV', 'frequencyA/RVRH', 'frequencyA/RVRV', 'frequencyA/VH', 'frequencyA/VV', 'frequencyA/RH', 'frequencyA/RV', 'frequencyB/validSamplesSubSwath2', 'frequencyB/validSamplesSubSwath3', 'frequencyB/validSamplesSubSwath4', 'frequencyB/HHHH', 'frequencyB/HHHV', 'frequencyB/HHVH', 'frequencyB/HHVV', 'frequencyB/HVHH', 'frequencyB/HVHV', 'frequencyB/HVVH', 'frequencyB/HVVV', 'frequencyB/VHHH', 'frequencyB/VHHV', 'frequencyB/VHVH', 'frequencyB/VHVV', 'frequencyB/VVHH', 'frequencyB/VVHV', 'frequencyB/VVVH', 'frequencyB/VVVV', 'frequencyB/RHRH', 'frequencyB/RHRV', 'frequencyB/RVRH', 'frequencyB/RVRV', 'frequencyB/VH', 'frequencyB/VV', 'frequencyB/RH', 'frequencyB/RV']"
-2022-05-02 18:51:40,542, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':327, "Warning: LSAR Metadata missing 1 fields: processingInformation/parameters/azimuthChirpWeighting"
-2022-05-02 18:51:40,554, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':384, "Warning: Invalid Orbit Number: 0"
-2022-05-02 18:51:40,560, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':544, "N/A: Checking frequencies for flist: {'A': <HDF5 group "/science/LSAR/RSLC/swaths/frequencyA" (15 members)>, 'B': <HDF5 group "/science/LSAR/RSLC/swaths/frequencyB" (15 members)>}"
-2022-05-02 18:51:40,567, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':485, "N/A: Start time b'2023-07-01T00:08:13.501873000', End time b'2023-07-01T00:08:21.501001000'"
-2022-05-02 19:00:52,490, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR A HH for zeros and NaNs"
-2022-05-02 19:00:52,496, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR A_HH has 348233 Zeros=0.1%"
-2022-05-02 19:01:05,199, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR A HV for zeros and NaNs"
-2022-05-02 19:01:05,205, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR A_HV has 349618 Zeros=0.1%"
-2022-05-02 19:01:08,444, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR B HH for zeros and NaNs"
-2022-05-02 19:01:08,450, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR B_HH has 2131840 Zeros=2.2%"
-2022-05-02 19:01:11,668, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR B HV for zeros and NaNs"
-2022-05-02 19:01:11,674, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR B_HV has 2134675 Zeros=2.2%"
-2022-05-02 19:01:11,849, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':197, "N/A: Found 4 images: dict_keys(['LSAR A HH', 'LSAR A HV', 'LSAR B HH', 'LSAR B HV'])"
-2022-05-02 19:03:42,604, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 0-th image: LSAR A HH"
-2022-05-02 19:03:53,893, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 1-th image: LSAR A HV"
-2022-05-02 19:04:04,618, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 2-th image: LSAR B HH"
-2022-05-02 19:04:07,374, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 3-th image: LSAR B HV"
-2022-05-02 19:04:10,059, DEBUG, QA, misc, 101000 '/projects/QualityAssurance/quality/SLCFile.py':266, "N/A: bounds_linear 0.2"
-2022-05-02 19:09:44,166, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':376, "N/A: File stats.h5 mode r+"
-2022-05-02 19:09:45,065, INFO, QA, misc, 100000 '/projects/QualityAssurance/verify_rslc.py':143, "N/A: Runtime = 1084 seconds"
-2022-05-02 19:24:21,158, INFO, QA, misc, 100000 '/projects/QualityAssurance/verify_rslc.py':73, "N/A: Successfully parsed XML file /projects/QualityAssurance/xml/nisar_L1_RSLC.xml"
-2022-05-02 19:24:21,161, INFO, QA, misc, 100000 '/projects/QualityAssurance/verify_rslc.py':79, "N/A: Opening file /home/niemoell/dat/fromJoanne_05022022/rslc_REE_testarea134/output_rslc/rslc.h5 with xml spec /projects/QualityAssurance/xml/nisar_L1_RSLC.xml"
-2022-05-02 19:24:21,165, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':37, "N/A: Opening file /home/niemoell/dat/fromJoanne_05022022/rslc_REE_testarea134/output_rslc/rslc.h5"
-2022-05-02 19:24:21,172, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':65, "N/A: LSAR Start time 2023-07-01T00:08:13.501873000"
-2022-05-02 19:24:21,174, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':65, "N/A: SSAR Start time 9999-99-99T99:99:99"
-2022-05-02 19:24:21,177, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':67, "N/A: Found band LSAR"
-2022-05-02 19:24:21,180, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/HdfGroup.py':23, "N/A: Initializing HDF Group LSAR Swath with /science/LSAR/RSLC/swaths"
-2022-05-02 19:24:21,183, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/HdfGroup.py':23, "N/A: Initializing HDF Group LSAR Metadata with /science/LSAR/RSLC/metadata"
-2022-05-02 19:24:21,186, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/HdfGroup.py':23, "N/A: Initializing HDF Group LSAR Identification with /science/LSAR/identification/"
-2022-05-02 19:24:21,189, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':64, "N/A: SSAR not present"
-2022-05-02 19:24:21,192, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':84, "N/A: Found LSAR FrequencyA"
-2022-05-02 19:24:21,196, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':84, "N/A: Found LSAR FrequencyB"
-2022-05-02 19:24:21,202, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':178, "N/A: Checking polarization  in <class 'h5py._hl.group.Group'>"
-2022-05-02 19:24:21,205, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':178, "N/A: Checking polarization  in <class 'h5py._hl.group.Group'>"
-2022-05-02 19:24:21,211, DEBUG, QA, misc, 101000 '/projects/QualityAssurance/quality/NISARFile.py':298, "N/A: nsubswaths <class 'int'> = 1"
-2022-05-02 19:24:21,215, DEBUG, QA, misc, 101000 '/projects/QualityAssurance/quality/NISARFile.py':298, "N/A: nsubswaths <class 'int'> = 1"
-2022-05-02 19:24:21,218, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':311, "N/A: LSAR: no_look=['frequencyA/validSamplesSubSwath2', 'frequencyA/validSamplesSubSwath3', 'frequencyA/validSamplesSubSwath4', 'frequencyA/HHHH', 'frequencyA/HHHV', 'frequencyA/HHVH', 'frequencyA/HHVV', 'frequencyA/HVHH', 'frequencyA/HVHV', 'frequencyA/HVVH', 'frequencyA/HVVV', 'frequencyA/VHHH', 'frequencyA/VHHV', 'frequencyA/VHVH', 'frequencyA/VHVV', 'frequencyA/VVHH', 'frequencyA/VVHV', 'frequencyA/VVVH', 'frequencyA/VVVV', 'frequencyA/RHRH', 'frequencyA/RHRV', 'frequencyA/RVRH', 'frequencyA/RVRV', 'frequencyA/VH', 'frequencyA/VV', 'frequencyA/RH', 'frequencyA/RV', 'frequencyB/validSamplesSubSwath2', 'frequencyB/validSamplesSubSwath3', 'frequencyB/validSamplesSubSwath4', 'frequencyB/HHHH', 'frequencyB/HHHV', 'frequencyB/HHVH', 'frequencyB/HHVV', 'frequencyB/HVHH', 'frequencyB/HVHV', 'frequencyB/HVVH', 'frequencyB/HVVV', 'frequencyB/VHHH', 'frequencyB/VHHV', 'frequencyB/VHVH', 'frequencyB/VHVV', 'frequencyB/VVHH', 'frequencyB/VVHV', 'frequencyB/VVVH', 'frequencyB/VVVV', 'frequencyB/RHRH', 'frequencyB/RHRV', 'frequencyB/RVRH', 'frequencyB/RVRV', 'frequencyB/VH', 'frequencyB/VV', 'frequencyB/RH', 'frequencyB/RV']"
-2022-05-02 19:24:21,236, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':327, "Warning: LSAR Metadata missing 1 fields: processingInformation/parameters/azimuthChirpWeighting"
-2022-05-02 19:24:21,249, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':384, "Warning: Invalid Orbit Number: 0"
-2022-05-02 19:24:21,253, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':544, "N/A: Checking frequencies for flist: {'A': <HDF5 group "/science/LSAR/RSLC/swaths/frequencyA" (15 members)>, 'B': <HDF5 group "/science/LSAR/RSLC/swaths/frequencyB" (15 members)>}"
-2022-05-02 19:24:21,261, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':485, "N/A: Start time b'2023-07-01T00:08:13.501873000', End time b'2023-07-01T00:08:21.501001000'"
-2022-05-02 19:33:28,390, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR A HH for zeros and NaNs"
-2022-05-02 19:33:28,396, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR A_HH has 348233 Zeros=0.1%"
-2022-05-02 19:33:41,694, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR A HV for zeros and NaNs"
-2022-05-02 19:33:41,700, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR A_HV has 349618 Zeros=0.1%"
-2022-05-02 19:33:45,085, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR B HH for zeros and NaNs"
-2022-05-02 19:33:45,091, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR B_HH has 2131840 Zeros=2.2%"
-2022-05-02 19:33:48,479, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/NISARFile.py':445, "N/A: Checking image LSAR B HV for zeros and NaNs"
-2022-05-02 19:33:48,485, WARNING, QA, misc, 102000 '/projects/QualityAssurance/quality/NISARFile.py':462, "Warning: LSAR B_HV has 2134675 Zeros=2.2%"
-2022-05-02 19:33:48,660, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':197, "N/A: Found 4 images: dict_keys(['LSAR A HH', 'LSAR A HV', 'LSAR B HH', 'LSAR B HV'])"
-2022-05-02 19:36:18,204, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 0-th image: LSAR A HH"
-2022-05-02 19:36:29,492, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 1-th image: LSAR A HV"
-2022-05-02 19:36:40,237, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 2-th image: LSAR B HH"
-2022-05-02 19:36:43,002, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':246, "N/A: Looking at 3-th image: LSAR B HV"
-2022-05-02 19:36:45,699, DEBUG, QA, misc, 101000 '/projects/QualityAssurance/quality/SLCFile.py':266, "N/A: bounds_linear 0.2"
-2022-05-02 19:42:09,117, INFO, QA, misc, 100000 '/projects/QualityAssurance/quality/SLCFile.py':376, "N/A: File stats.h5 mode r+"
-2022-05-02 19:42:09,972, INFO, QA, misc, 100000 '/projects/QualityAssurance/verify_rslc.py':143, "N/A: Runtime = 1068 seconds"
-PLACEHOLDER FILE ONLY -- NOT ACTUAL LOG OUTPUTS
+LOG_TXT = """PLACEHOLDER FILE -- NOT ACTUAL LOG OUTPUTS
+<time-tag>, <log level>, <workflow>, <module>, <error code>, <error location>, <logged message>
+PLACEHOLDER FILE -- NOT ACTUAL LOG OUTPUTS
 """
 
 SUMMARY_CSV = """Tool,Check Description,Result,Threshold,Actual,Notes
 QA,Able to open NISAR input file?,PASS,,,PLACEHOLDER
-QA,Input file validation successful?,FAIL,,,PLACEHOLDER Invalid Start/End Time(s).
-QA,0 missing fields per product spec?,FAIL,0,4,PLACEHOLDER Missing: "/science/LSAR/RSLC/swaths/frequencyA/validSamplesSubSwath3".
-QA,Only LSAR and/or SSAR band found?,PASS,,,PLACEHOLDER Found: LSAR.
-QA,Only Frequency A and/or B found?,PASS,,,PLACEHOLDER Found: A.
-QA,> 0 valid images found?,PASS,1,2,PLACEHOLDERFound: LSAR_A_HH. LSAR_A_HV.
-QA,Contains 4 subswath bounds for FreqA?,FAIL,4,3,PLACEHOLDER		
-QA,Contains 4 subswath bounds for FreqB?,PASS,n/a,n/a,PLACEHOLDER FreqB not present in file.
-QA,% invalid pixels < 80%?,PASS,80%,1.7%,PLACEHOLDER # invalid pixels: 250/15000.
-QA,% zero pixels < 98%?,FAIL,98%,93.9%,PLACEHOLDER # zero pixels: 14090/15000.
 """
 
 KML_FILE = """<?xml version="1.0" encoding="UTF-8"?>
